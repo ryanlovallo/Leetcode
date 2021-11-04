8a7b296bb43a6520897c0d0ed74a52b96c5eb1fd
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-04 11:05:13 -0400

    Time: 184 ms (69.00%), Space: 62.1 MB (34.92%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(), points.end(), [](const vector<int> &a, const vector<int> &b) {
-            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
+        nth_element(points.begin(), points.begin()+k, points.end(), [](vector<int> &a, vector<int> b) {
+            return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]); 
         });
-        
-        return vector<vector<int>> (points.begin(), points.begin()+k);
-        
+        points.resize(k);
+        return points;
     }
 };
