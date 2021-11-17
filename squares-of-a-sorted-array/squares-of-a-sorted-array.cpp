d10a1496da07693f73dd18548bec691302f728f7
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-17 12:56:43 -0500

    Time: 24 ms (92.96%), Space: 26 MB (55.07%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,39 +1,13 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        /*
-        find pivot point
-        2 pointer, compare abs values,
-        */
-        if (nums[0] >= 0 || nums.back() < 0) {
-            bool revr = nums.back() < 0;
-            for(int &n : nums) n *= n;
-            if (revr) reverse(nums.begin(), nums.end());
-            return nums;
-        }
+        vector<int> res(nums.size());
         
-        // pivot
-        vector<int> res;
-        res.reserve(nums.size());
-        
-        // find pivot
-        int lpivot, rpivot;
-        for(int i = 0; i < nums.size()-1; ++i) {
-            if (nums[i] < 0 && nums[i+1] >= 0) {
-                lpivot = i;
-                break;
-            }
-        }
-        rpivot = lpivot+1;
-        while (lpivot >= 0 || rpivot < nums.size()) {
-            if (lpivot < 0) res.push_back(pow(nums[rpivot++],2));
-            else if (rpivot >= nums.size()) res.push_back(pow(nums[lpivot--],2));
-            else {
-                if (abs(nums[lpivot]) >= abs(nums[rpivot])) res.push_back(pow(nums[rpivot++],2));
-                else res.push_back(pow(nums[lpivot--],2)); 
-            }
+        int l = 0, r = nums.size()-1;
+        for (int i = nums.size()-1; i >= 0; --i) {
+            res[i] = (abs(nums[r]) > abs(nums[l])) ? pow(nums[r--],2) : pow(nums[l++],2);
         }
         return res;
-        
     }
+    
 };
