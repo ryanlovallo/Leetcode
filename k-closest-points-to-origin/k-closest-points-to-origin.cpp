d2c392f873c37b8c7c4c3646a3bf9605c293cb36
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-23 15:34:35 -0500

    Time: 158 ms (77.23%), Space: 49.3 MB (88.67%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,10 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        nth_element(points.begin(), points.begin()+k, points.end(), [](vector<int> &a, vector<int> b) {
-            return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]); 
-        });
+        int l = 0, r = points.size()-1, mid;
+        
+        while (l < r) {
+            mid = partition(points, l, r);
+            
+            if (mid == k) {
+                break;    
+            }
+            if (mid >= k) {
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
         points.resize(k);
         return points;
+        
     }
+    
+    int partition(vector<vector<int>>& points, int left, int right) {
+        if (left >= right ) return left;
+        int p = left;
+        int pointDist = dist(points[right][0], points[right][1]);
+        
+        for (int i = left; i < right; ++i) {
+            if (dist(points[i][0], points[i][1]) < pointDist) {
+                swap(points[p], points[i]);
+                p++;
+            } 
+        }
+        swap(points[right], points[p]);
+        return p;
+    }
+    
+    int dist(int x, int y) {
+        return x*x + y*y;
+    }
+    
+    
 };
