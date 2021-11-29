885a73fd0b38256851262ebf9dfdb7440e038592
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-29 16:01:24 -0500

    Time: 132 ms (72.41%), Space: 68.8 MB (31.31%) - LeetHub

--- buildings-with-an-ocean-view/buildings-with-an-ocean-view.cpp
+++ buildings-with-an-ocean-view/buildings-with-an-ocean-view.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
-        int rightMax = INT_MIN;
+        int curMax = INT_MIN;
         vector<int> res;
+        
         for (int i = heights.size()-1; i >= 0; --i) {
-            if (heights[i] > rightMax) res.push_back(i);
-            rightMax = max(rightMax, heights[i]);
+            if (heights[i] > curMax) {
+                res.push_back(i);
+                curMax = heights[i];
+            }
         }
         reverse(res.begin(), res.end());
         return res;
