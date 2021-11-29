6bb170e00559ba8214ab01fd76f0de43ca106b36
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-29 16:27:41 -0500

    Time: 0 ms (100.00%), Space: 6.2 MB (77.60%) - LeetHub

--- minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int lefts = 0, stack = 0;
+        int emptyRights = 0, numLefts = 0;
         
-        for (char c : s) {
-            if (c == '(') stack++;
-            else if (c == ')' && stack == 0) lefts++;
-            else stack--;
+        for (char& c : s) {
+            if (c == '(') numLefts++;
+            else if (c == ')' && numLefts > 0) numLefts--;
+            else emptyRights++;
         }
-        return lefts + stack;
+        return emptyRights + numLefts;
     }
 };
