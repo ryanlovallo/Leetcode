9e664e668cb1a0393a16fcd205624e8f7c9ee5f4
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-29 20:01:02 -0500

    Time: 8 ms (47.90%), Space: 6.9 MB (42.56%) - LeetHub

--- add-strings/add-strings.cpp
+++ add-strings/add-strings.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        string res = "";
-        int n1 = num1.size()-1, n2 = num2.size()-1, carry = 0, n;
+        int i1 = num1.size()-1, i2 = num2.size()-1, carry = 0, cur;
+        string res;
         
-        while (n1 >= 0 || n2 >= 0) {
-            if (n1 < 0) {
-                n = int(num2[n2] - '0') + carry;
-                n2--;
-            } else if (n2 < 0) {
-                n = int(num1[n1] - '0') + carry;
-                n1--;
+        while (i1 >= 0 || i2 >= 0) {
+            if (i1 < 0) {
+                cur = ((num2[i2] - '0') + carry) % 10;
+                carry = ((num2[i2--] - '0') + carry) / 10;
+            } else if (i2 < 0) {
+                cur = ((num1[i1] - '0') + carry) % 10;
+                carry = ((num1[i1--] - '0') + carry) / 10;
             } else {
-                n = int(num1[n1--] - '0') + int(num2[n2--] - '0') + carry;
+                int c = (num1[i1--] - '0') + (num2[i2--] - '0') + carry;
+                cur = c % 10;
+                carry = c / 10;
             }
-            res += char('0' + (n % 10));
-            carry = n / 10;
+            res += to_string(cur);
         }
-        if (carry > 0) res += "1";
+        if (carry) res += to_string(carry);
         reverse(res.begin(), res.end());
         return res;
+        // reverse
     }
 };
+
+// 4 5 6
+//   7 7
+
+// 3 
