b1f1d050437b191c7c685a7bb38a641778f6ecae
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-30 14:41:35 -0500

    Time: 16 ms (89.96%), Space: 14.2 MB (55.62%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b) {
-           return (a[0] != b[0]) ? a[0] < b[0] : a[1] < b[1]; 
-        });
+        sort(intervals.begin(), intervals.end());
         
-        vector<vector<int>> res;
-        
-        int i = 0, maxEnd = 0, minStart = 0;
-        
-        while (i < intervals.size()) {
-            maxEnd = intervals[i][1];
-            minStart = intervals[i][0];
-            if (i == intervals.size()-1) {
-                res.push_back({intervals[i][0],intervals[i][1]});
-                break;
+        vector<vector<int>> merged;
+        for (vector<int>& invl : intervals) {
+            if (merged.empty() || merged.back()[1] < invl[0]) {
+                merged.push_back(invl);
+            } else {
+                merged.back()[1] = max(invl[1], merged.back()[1]);
             }
-            while (i < intervals.size()-1) {
-                if (intervals[i+1][0] > maxEnd) break;
-                maxEnd = max(maxEnd, intervals[i+1][1]);
-                ++i;
-            } 
-            res.push_back({minStart, maxEnd});
-            ++i;
         }
-        return res;
-        
+        return merged;
     }
 };
-
-/*
-[[2,3],[2,6],[1,6],[8,10],[15,18]]
-[[1,6],[2,3],[2,6],[4,7],[8,10],[15,18]]
-*/
