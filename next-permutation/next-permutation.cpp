059037d553c978275ddd165a93070cbd77e147fd
Ryan Lovallo <ryanlovallo@gmail.com>
2021-11-30 14:28:53 -0500

    Time: 4 ms (75.69%), Space: 12 MB (74.54%) - LeetHub

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        if (nums.size() == 1) return;
-        int rev;
+        if (nums.size()==1) return;
+        
+        int peakIndex = -1;
         for (int i = nums.size()-1; i > 0; --i) {
-            if (nums[i] <= nums[i-1]) continue;
-            
-            int indx = i;
-            
-            while (indx < nums.size()) {
-                if (nums[indx] <= nums[i-1]) break;
-                indx++;
-            } 
-            indx--; 
-            
-            swap (nums[i-1], nums[indx]);
-            rev = i;
-            break;
+            if (nums[i] > nums[i-1]) {
+                peakIndex = i-1;
+                break;
+            }
         }
         
-        reverse(nums.begin() + rev, nums.end());
+        if (peakIndex == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        int swapIndex = -1;
+        for (int i = peakIndex+1; i < nums.size(); ++i) {
+            if (nums[i] <= nums[peakIndex]) {
+                swapIndex = i-1;
+                break;
+            }
+        }
+        if (swapIndex == -1) swapIndex = nums.size()-1;
+        
+        swap(nums[peakIndex], nums[swapIndex]);
+        reverse(nums.begin() + peakIndex + 1, nums.end());
+        
     }
 };
 
-/*
-5 2 6 4 3
-
-5 3 2 4 6
-*/
+// 1 3 5 3 2
