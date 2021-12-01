7897566f097cd39977582d81fa7652537bb03f02
Ryan Lovallo <ryanlovallo@gmail.com>
2021-12-01 10:04:28 -0500

    Time: 20 ms (89.25%), Space: 11.7 MB (42.73%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int> indexes;
+        stack<int> st;
         
         for (int i = 0; i < s.size(); ++i) {
             if (isalpha(s[i])) continue;
-            if (s[i] == '(') indexes.push(i);
-            else if (indexes.empty()) s[i] = '!';
-            else indexes.pop();
-        } 
-        while (!indexes.empty()) {
-            s[indexes.top()] = '!';
-            indexes.pop();
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')' && st.empty()) {
+                s[i] = '!';
+            } else {
+                st.pop();
+            }
+        }
+        while (!st.empty()) {
+            s[st.top()] = '!';
+            st.pop();
         }
         
         string res;
@@ -20,7 +24,6 @@ public:
             if ((s[i]) == '!') continue;
             res += s[i];
         }
-
         return res;
     }
 };
